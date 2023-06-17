a5bedd4059145dde4cd5db8fb091361f1b8d119c
gillama <marina.giljanovic@gmail.com>
2023-06-17 17:11:04 +0200

    Custom object icon

--- src/store/layers/ObjectsLayer.js
+++ src/store/layers/ObjectsLayer.js
@@ -2,15 +2,26 @@ import GeoJSON from 'ol/format/GeoJSON';
 import VectorLayer from 'ol/layer/Vector';
 import { Cluster } from 'ol/source';
 import VectorSource from 'ol/source/Vector';
+import Icon from 'ol/style/Icon';
+import Style from 'ol/style/Style';
 
 const objectsLayer = new VectorLayer({
     updateWhileAnimating: true,
     source: new Cluster({
-        distance: 20,
+        distance: 40,
         minDistance: 0,
         source: new VectorSource({
             url: 'https://plovput.li-st.net/getObjekti/',
             format: new GeoJSON({ featureProjection: 'EPSG:3857' })
+        })
+    }),
+    style: new Style({
+        image: new Icon({
+            color: 'white',
+            scale: 0.05,
+            crossOrigin: 'anonymous',
+            src: 'images/object.svg',
+            anchor: [0.5, 1]
         })
     })
 });
